d88321f453bbc8189245eab635d1b86ee0b7c984
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-08-30 17:11:49 -0700

    gif-search

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000!');
+});
+
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
+
+var giphy = require('giphy-api')();
+
+
+app.get('/', function (req, res) {
+    giphy.search(req.query.term, function (err, response) {
+        res.render('home', {gifs: response.data})
+      });
+    });
+app.use(express.static('public'));
